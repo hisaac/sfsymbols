db2dea168679a710a80af06d1f9025517becea16
Isaac Halvorson <hello@hisaac.net>
2021-02-19 16:05:54 -0600

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,14 +9,9 @@ let package = Package(
         .executable(name: "sfsymbols", targets: ["sfsymbols"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.2")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which
-        // this package depends on.
         .target(name: "SFSymbolsCore", dependencies: []),
         .target(name: "sfsymbols", dependencies: ["ArgumentParser", "SFSymbolsCore"])
     ]
